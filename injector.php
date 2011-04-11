f4022f0ae73bd70eeb9050aef389a520279ab412
Henri Bergius <henri.bergius@iki.fi>
2011-04-11 17:06:21 +0300

    Register observers in manifest instead of injector

--- injector.php
+++ injector.php
@@ -1,42 +1,6 @@
 <?php
 class org_midgardproject_projectsite_injector
 {
-    public function inject_process(midgardmvc_core_request $request)
-    {
-        static $connected = false;
-        if ($connected)
-        {
-            return;
-        }
-        
-        // Subscribe to content changed signals from Midgard
-        midgard_object_class::connect_default
-        (
-            'org_midgardproject_projectsite_product',
-            'action-create',
-            'org_midgardproject_projectsite_injector::create_url'
-        );
-        midgard_object_class::connect_default
-        (
-            'org_midgardproject_projectsite_product',
-            'action-update',
-            'org_midgardproject_projectsite_injector::create_url'
-        );
-        midgard_object_class::connect_default
-        (
-            'org_midgardproject_projectsite_document',
-            'action-create',
-            'org_midgardproject_projectsite_injector::check_product'
-        );
-        midgard_object_class::connect_default
-        (
-            'org_midgardproject_projectsite_download',
-            'action-create',
-            'org_midgardproject_projectsite_injector::check_product'
-        );
-        $connected = true;
-    }
-
     public function inject_template(midgardmvc_core_request $request)
     {
         // We inject the template to provide MeeGo styling
